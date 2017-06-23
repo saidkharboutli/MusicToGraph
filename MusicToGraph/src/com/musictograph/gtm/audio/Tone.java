96666c3bbbc204b8aa8cdfac49d42487b4b33412
Sa'id <drago7654321@gmail.com>
2017-06-22 22:29:03 -0400

    Replaced Beeper with Princeton's Tone and StdAudio

---
+++ MusicToGraph/src/com/musictograph/gtm/audio/Tone.java
@@ -0,0 +1,12 @@
+package com.musictograph.gtm.audio;
+
+public class Tone {
+	public static double[] tone(double hz, double duration) { 
+        int n = (int) (44100 * duration);
+        double[] a = new double[n+1];
+        for (int i = 0; i <= n; i++) {
+            a[i] = Math.sin(2 * Math.PI * i * hz / 44100);
+        }
+        return a; 
+    }
+}
--- MusicToGraph/src/com/musictograph/main/Driver.java
+++
@@ -1,8 +0,0 @@
-package com.musictograph.main;
-
-public class Driver {
-	public static void main(String args[])
-	{
-		
-	}
-}
