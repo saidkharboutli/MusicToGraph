153bacd7de5372aea12257d828bcb96aad06d45b
Sa'id <drago7654321@gmail.com>
2017-06-22 22:29:37 -0400

    Added driver for Tone/StdAudio

---
+++ MusicToGraph/src/com/musictograph/main/ToneDriver.java
@@ -0,0 +1,18 @@
+package com.musictograph.main;
+
+import javax.sound.sampled.LineUnavailableException;
+
+import com.musictograph.gtm.audio.StdAudio;
+import com.musictograph.gtm.audio.Tone;
+
+public class ToneDriver {
+	public static void main(String args[]) throws LineUnavailableException
+	{
+		for(int hz = 290; hz <= 490; hz+=15.5)
+		{
+	        double duration = .25;
+	        double[] a = Tone.tone(hz, duration);
+	        StdAudio.play(a);
+		}
+	}
+}
