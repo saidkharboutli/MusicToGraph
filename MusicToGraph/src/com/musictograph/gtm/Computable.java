69338963eb0ae953510fb27b6196b7bcdad60f2e
Sa'id <drago7654321@gmail.com>
2017-06-23 00:24:43 -0400

    implemented constructor

--- MusicToGraph/src/com/musictograph/gtm/Computable.java
+++ MusicToGraph/src/com/musictograph/gtm/Computable.java
@@ -1,5 +1,39 @@
 package com.musictograph.gtm;
 
-public class Computable {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Computable {
+	double coeffecients[];
+	double derivative[];
+	
+	public Computable(String function, boolean polynomial)
+	{
+		if(polynomial) {
+			List<String> terms = Arrays.asList(function.replace(" ", "").split("\r[+-]"));
+			for(int c = 0; c < terms.size(); c++)
+			{
+				String term[] = terms.get(c).split("^");
+				if(term.length > 1) coeffecients[c] = Double.parseDouble(terms.get(0).replace("x", ""));
+			}
+			
+			for(int i = 0; i < coeffecients.length-1; i++) 
+				derivative[i] = coeffecients[i]*(coeffecients.length-i-1);
+			
+		} else {
+			//TODO: graph by increments of .25, find max and min (or one of them)
+			//TODO: and then use those values.
+		}
+	}
+	
+	public static String expandFunc(String simplifiedFunc)
+	{
+		String expanded = "";
+		
+		
+		
+		return expanded;
+	}
+	
 }
