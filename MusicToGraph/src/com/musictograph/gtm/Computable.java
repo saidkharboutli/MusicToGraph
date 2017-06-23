ca1988d82a9c07e6219ade9198175f6f2bc21dc4
Sa'id <drago7654321@gmail.com>
2017-06-23 04:01:11 -0400

    added toString and implemented test driver

--- MusicToGraph/src/com/musictograph/gtm/Computable.java
+++ MusicToGraph/src/com/musictograph/gtm/Computable.java
@@ -16,10 +16,8 @@ public class Computable {
 				if (term.length > 1)
 					coeffecients[c] = Double.parseDouble(terms.get(0).replace("x", "").replace("+", ""));
 			}
-
 			for (int i = 0; i < coeffecients.length - 1; i++)
 				derivative[i] = coeffecients[i] * (coeffecients.length - i - 1);
-
 		} else {
 			// TODO: graph by increments of .25, find max and min (or one of
 			// them)
@@ -30,27 +28,43 @@ public class Computable {
 	public static List<String> expandFunc(List<String> simplifiedFunc) {
 		List<String> expanded = new ArrayList<String>();
 
-		//sort by degree
+		// sort by degree
 		for (int c = 0; c < expanded.size() - 1; c++) {
-			if(expanded.get(c).split("^").length <= 1) {
+			if (expanded.get(c).split("^").length <= 1) {
 				expanded.add(expanded.get(c));
 				expanded.remove(expanded.get(expanded.size() - 1));
-			} else if((expanded.get(c).contains("^") && expanded.get(c + 1).contains("^"))
-					&& (Integer.parseInt(expanded.get(c).split("^")[1]) < Integer.parseInt(expanded.get(c + 1).split("^")[1]))) {
+			} else if ((expanded.get(c).contains("^") && expanded.get(c + 1).contains("^")) && (Integer
+					.parseInt(expanded.get(c).split("^")[1]) < Integer.parseInt(expanded.get(c + 1).split("^")[1]))) {
 				String temp = expanded.get(c);
 				expanded.set(c, expanded.get(c + 1));
 				expanded.set(c + 1, temp);
 			}
 		}
-		
-		//find missing terms
-		for(int c = 0; c < expanded.size() - 2; c++)
-		{
-			if((Integer.parseInt(expanded.get(c).split("^")[1]) - 1) != Integer.parseInt(expanded.get(c + 1).split("^")[1])) {
+
+		// find missing terms
+		for (int c = 0; c < expanded.size() - 2; c++) {
+			if ((Integer.parseInt(expanded.get(c).split("^")[1]) - 1) != Integer
+					.parseInt(expanded.get(c + 1).split("^")[1])) {
 				expanded.add(c + 1, "0x^" + expanded.get(c).split("^")[1]);
 			}
 		}
 		return expanded;
+	}
+
+	// public static double minMax()
+	// {
+	//
+	// }
+	//
+	// public static int check()
+	// {
+	//
+	// }
+
+	public String toString() {
+		String ret = "Coeffecients: " + Arrays.toString(coeffecients) + "\n" + "Derivatives: "
+				+ Arrays.toString(derivative);
+		return ret;
 	}
 
 }
---
+++ MusicToGraph/src/com/musictograph/gtm/main/ComputableDriver.java
@@ -0,0 +1,10 @@
+package com.musictograph.gtm.main;
+
+import com.musictograph.gtm.Computable;
+
+public class ComputableDriver {
+	public static void main(String args[])
+	{
+		Computable computable = new Computable("x^2", true);
+	}
+}
